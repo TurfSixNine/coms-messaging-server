907c2390a32c3b9757371c255d404d53f0e784c7
ayor <dosumuayomide@gmail.com>
2024-05-05 09:37:01 +0100

    implement ssl on application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes import Routes
 from flask_cors import CORS
 from services.db import mongo
 from gevent.pywsgi import WSGIServer
-
+import ssl
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -18,8 +18,12 @@ Routes(api)
 if __name__ == "__main__":
     app.config.from_object('settings')
     mongo.init_app(app)
+    #setting up ssl:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain('/etc/pki/tls/certs/csr.pem', '/etc/pki/tls/certs/custom.key')
+
     print("running the application")
-    http_server = WSGIServer(('', 5001), app)
+    http_server = WSGIServer(('', 5001), app, ssl_context=context)
     http_server.serve_forever()
 
     
